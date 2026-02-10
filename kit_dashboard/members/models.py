1b7c6523f678a94e32b51c778d734272c162b933
Sarah Hooper <shooper611@gmail.com>
2026-02-10 12:49:24 -0800

    base done

--- kit_dashboard/members/models.py
+++ kit_dashboard/members/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Kit(models.Model):
+    name = models.CharField(max_length=120)
+    issues = models.TextField(blank=True, null=True)
+    needs_restock = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PostMortem(models.Model):
+    kit = models.ForeignKey(Kit, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120, blank=True, null=True)
+    event_name = models.CharField(max_length=120)
+    event_date = models.DateField()
+    summary = models.TextField()
+    restock = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.event_name
--- kit_dashboard/members/views.py
+++ kit_dashboard/members/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 # Create your views here.
 def members(request):
-    return HttpResponse("Hello, world. You're at the kit dashboard members view.")
+    return render(request, "home.html")
